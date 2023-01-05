84303410c14050fc9dc0436edbe32d9fd38c14bb
Елена Вернер <gu-helen04@mail.ru>
2023-01-06 04:00:23 +0500

    first commit_

---
+++ m2m-relations/articles/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+import collections
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count_main_topics = 0
+        list_unic = []
+        for form in self.forms:
+            if form.cleaned_data:
+                if form.cleaned_data['is_main']:
+                    count_main_topics += 1
+                list_unic.append(form.cleaned_data['tag'])
+        list_not_unic = [item for item, count in collections.Counter(list_unic).items() if count > 1]
+        if len(list_not_unic) > 0:
+            raise ValidationError('Тэги должны быть уникальными')
+        if count_main_topics == 0:
+            raise ValidationError('Должен быть хотя бы один основной раздел статьи')
+        if count_main_topics > 1:
+            raise ValidationError('Основным может быть только один раздел')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'published_at', 'str_tags']
+    list_filter = ['scopes']
+    ordering = ['title', 'published_at']
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    list_filter = ['name']
---
+++ m2m-relations/articles/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField('Tag', through='Scope', verbose_name='Тэги')
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+    def str_tags(self):
+        return ', '.join([scope.tag.name for scope in self.scopes.all()[:3]])
+
+
+class Scope(models.Model):
+    article = models.ForeignKey('Article', on_delete=models.CASCADE, related_name='scopes', verbose_name='Статья')
+    tag = models.ForeignKey('Tag', on_delete=models.CASCADE, related_name='scopes', verbose_name='Тэг')
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
+    class Meta:
+        verbose_name = 'Тег статьи'
+        verbose_name_plural = 'Теги статей'
+        ordering = ['-is_main']
